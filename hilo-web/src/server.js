e1ec9c385155f707119e330c76fcacf493742fdb
Auuming <148842078+Auuming@users.noreply.github.com>
2024-04-14 22:17:40 +0700

    version ไม่ใช้ path

---
+++ hilo-web/src/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const PORT = 3000;
+
+//controllers
+const viewController = require('./controllers/viewController');
+const storeUserController = require('./controllers/storeUserController');
+
+// Middleware to parse request body
+//app.use(express.urlencoded({ extended: false }));
+// when doing backend chat don't recommend this
+
+//.env
+require('dotenv').config();
+
+//Mongodb connectios
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true});
+
+// Define routes
+app.get('/', viewController.home);
+app.get('/game', viewController.game);
+app.get('/signin', viewController.signin);
+app.get('/login', viewController.login);
+app.post('/signinForm', storeUserController);
+
+// Serve static files (like CSS and JavaScript)
+app.use(express.static(__dirname + '/views/home-page'));
+app.use(express.static(__dirname + '/views/game-page'));
+app.use(express.static(__dirname + '/views/signin-page'));
+app.use(express.static(__dirname + '/views/login-page'));
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
