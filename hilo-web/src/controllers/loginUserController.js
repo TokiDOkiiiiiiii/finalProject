091e9412a940bdf2af386703f5f56c696ee5801c
Jaiigh <jayjaycc2004@gmail.com>
2024-04-19 16:10:06 +0700

    user control chnage

---
+++ hilo-web/src/controllers/loginUserController.js
@@ -0,0 +1,46 @@
+const User = require('../models/User');
+const activeSessions = require('../server');
+const { setUser } = require('./getUserController');
+
+function generateSessionId() {
+    // Generate a random string for session ID
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const sessionIdLength = 20;
+    let sessionId = '';
+    for (let i = 0; i < sessionIdLength; i++) {
+        sessionId += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return sessionId;
+}
+
+module.exports = {
+    loginFunction: (req, res, callback) => {
+        const { username, password } = req.body;
+
+        User.findOne({ username: username, password: password }).then((foundedUser) => {
+            if (foundedUser) {
+                // Set the value of user
+                module.exports.user = foundedUser;
+
+                // Generate a unique session ID
+                const sessionId = generateSessionId();
+                // Store session ID in activeSessions
+                activeSessions[sessionId] = { userId: foundedUser._id };
+                // Set session ID in cookie
+                res.cookie('sessionId', sessionId, { httpOnly: true });
+
+                // Call the callback function with foundedUser as an argument
+                callback(null, foundedUser);
+                setUser(foundedUser);
+
+                res.redirect('/');
+            } else {
+                //return res.status(400).send("User not found");
+                res.redirect('/login');
+            }
+        }).catch(err => {
+            console.error(err);
+            return res.status(500).send("Internal Server Error");
+        });
+    }
+};
--- hilo-web/src/models/User.js
+++ hilo-web/src/models/User.js
@@ -9,7 +9,39 @@ const UserSchema = new Schema({
     password: {
         type: String,
         required: [true]
-    }
+    },
+    Score: {
+        type: Number,
+        default: 0
+    },
+    Ranking: {
+        type: Number,
+        default: 0
+    },
+    rollingTime: {
+        type: Number,
+        default: 0
+    },
+    highMul: {
+        type: Number,
+        default: 0
+    },
+    lowMul: {
+        type: Number,
+        default: 0
+    },
+    hiloMul: {
+        type: Number,
+        default: 0
+    },
+    baseAdd: {
+        type: Number,
+        default: 0
+    },
+    autoDice: {
+        type: Number,
+        default: 0
+    },
 });
 
 const User = mongoose.model('User', UserSchema);
